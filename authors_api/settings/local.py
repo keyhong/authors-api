bb288beb539b3419b8f7fda3afa45ad811b52e84
keyhong <wnhong96@gmail.com>
2023-12-25 14:21:56 +0900

    chore: setup basic django project

---
+++ authors_api/settings/local.py
@@ -0,0 +1,14 @@
+from .base import * #noqa
+from .base import env
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="TaSphqOrhSmrwV-JBMMgDSKV3nhm3Jzn_cYZMc4jbLvP8zKsbKg",
+)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# 사이트 간 요청을 허용하는 도메인 목록읻   
+CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
---
+++ authors_api/settings/production.py
@@ -0,0 +1,9 @@
+from .base import * #noqa
+from .base import env
+
+ADMINS = [
+    ("Gyuwon Hong", "wnhong96@gmail.com")
+]
+
+# TODO add domain names of the production server
+CSRF_TRUSTED_ORIGINS = [""]
